e8bc598fd23870a53ecabeb3c24ebc3351bb749e
Ujjwal <ukarora9@gmail.com>
2024-09-09 17:12:16 +0530

    Initial Commit

---
+++ messenger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  messenger
+//
+//  Created by Ujjwal Arora on 09/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
